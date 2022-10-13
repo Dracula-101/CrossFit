420e2824be9b2beab89430181f1e1cc786006e3c
Pratik Pujari <pratikpujari1000@gmail.com>
2022-10-13 07:55:59 +0530

    Theme Changes

---
+++ lib/styles/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Color lightPurple = const Color(0xFFA6A6A6);
+Color mildPurple = const Color(0xFF512DA8);
+Color darkPurple = const Color(0xFF443C5B);
+Color lightRed = const Color(0xFFF9435A);
+Color darkRed = const Color(0xFFFD3141);
+Color brightRed = const Color(0xFFB00020);
+Color darkGrey = const Color(0xFF303030);
+Color darkGreyContrast = const Color(0xFF424242);
+Color verylightGrey = const Color(0xBDBDBDBD);
+Color lightGrey = const Color(0x1FFFFFFF);
+Color lightGreyContrast = const Color(0x1FFFFFFF);
+Color dullGrey = const Color(0xFF616161);
+Color dullGreyContrast = const Color(0x62FFFFFF);
+Color grey = const Color(0x66C8C8C8);
+Color white = const Color(0xFFFFFFFF);
+Color dullWhite = const Color(0xFFF5F5F5);
